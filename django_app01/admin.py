9845d193ae95ffe923694ce708b955b839791af8
Oleg <olegdonezk@gmail.com>
2026-05-25 16:10:44 +0200

    Added inline forms, short titles and admin action for subtasks

--- django_app01/admin.py
+++ django_app01/admin.py
@@ -6,6 +6,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name',)
 
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
+
+@admin.action(description='Mark selected subtasks as Done')
+def mark_as_done(modeladmin, request, queryset):
+    queryset.update(status='Done')
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
@@ -15,6 +23,14 @@ class TaskAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     filter_horizontal = ('categories',)
 
+    inlines = [SubTaskInline]
+
+    def short_title(self, obj):
+        if len(obj.title) > 10:
+            return f'{obj.title[:10]}...'
+        return obj.title
+
+    short_title.short_description = 'Title'
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
@@ -22,3 +38,5 @@ class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'task', 'status', 'deadline', 'created_at')
     list_filter = ('status', 'created_at', 'task')
     search_fields = ('title', 'description')
+
+    actions = [mark_as_done]
